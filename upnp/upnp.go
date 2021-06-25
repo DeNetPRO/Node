0a7832727ba9c68197ff3792383c942321450cc4
RmailZ <elconejogato@gmail.com>
2021-06-25 10:58:21 +0300

    code refactoring

--- upnp/upnp.go
+++ upnp/upnp.go
@@ -3,6 +3,7 @@ package upnp
 import (
 	"context"
 	"dfile-secondary-node/shared"
+	"fmt"
 
 	"gitlab.com/NebulousLabs/go-upnp"
 )
@@ -11,6 +12,9 @@ var InternetDevice *upnp.IGD
 
 func InitIGD() {
 	const logInfo = "shared.InitIGD->"
+
+	fmt.Println("Enabling UPnP...")
+
 	device, err := upnp.DiscoverCtx(context.Background())
 	if err != nil {
 		shared.LogError(logInfo, shared.GetDetailedError(err))
