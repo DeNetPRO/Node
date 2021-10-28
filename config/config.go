35873f1454ddcc36f346156771870c503d4dcecb
RmailZ <elconejogato@gmail.com>
2021-10-28 14:41:21 +0300

    added network check

--- config/config.go
+++ config/config.go
@@ -154,10 +154,14 @@ func SelectNetwork() (string, error) {
 
 	fmt.Println("Choose a network")
 
-	networks := [2]string{"kovan", "mumbai"}
+	currentNets := make(map[int]string)
 
-	for i, network := range networks {
-		fmt.Println(i+1, network)
+	indx := 1
+
+	for network := range blckChain.Networks {
+		fmt.Println(indx, network)
+		currentNets[indx] = network
+		indx++
 	}
 
 	for {
@@ -167,25 +171,18 @@ func SelectNetwork() (string, error) {
 			return "", logger.CreateDetails(location, err)
 		}
 
-		netNum, err := strconv.Atoi(number)
+		netIndx, err := strconv.Atoi(number)
 		if err != nil {
 			fmt.Println("Incorrect value, try again")
 			continue
 		}
 
-		if netNum < 1 || netNum > len(networks) {
+		if netIndx < 1 || netIndx > indx-1 {
 			fmt.Println("Incorrect value, try again")
 			continue
 		}
 
-		netwrok := networks[netNum-1]
-
-		_, netExists := blckChain.Networks[netwrok]
-
-		if !netExists {
-			fmt.Println("Network is not supported")
-			continue
-		}
+		netwrok := currentNets[netIndx]
 
 		return netwrok, nil
 	}
